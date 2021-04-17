03c580adc901a6b9f27792342f4b568db1e80f15
fan777 <fan777@gmail.com>
2021-04-16 22:39:41 -0700

    restored

--- models.py
+++ models.py
@@ -6,9 +6,11 @@ db = SQLAlchemy()
 
 bcrypt = Bcrypt()
 
+
 def connect_db(app):
-  db.app = app
-  db.init_app(app)
+    db.app = app
+    db.init_app(app)
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -17,7 +19,8 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
-    feedback = db.relationship('Feedback', backref='user', cascade='all, delete')
+    feedback = db.relationship(
+        'Feedback', backref='user', cascade='all, delete')
 
     @classmethod
     def register(cls, username, pwd, email, first_name, last_name):
@@ -45,9 +48,11 @@ class User(db.Model):
         else:
             return False
 
+
 class Feedback(db.Model):
     __tablename__ = 'feedback'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullabe=False)
-    username = db.Column(db.String(20), db.ForeignKey('users.username'), nullable=False)
+    username = db.Column(db.String(20), db.ForeignKey(
+        'users.username'), nullable=False)
