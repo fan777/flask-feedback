ff120722cd2f228ebedfce7050167dad6495af8c
fan777 <fan777@gmail.com>
2021-04-16 23:14:13 -0700

    updates

--- models.py
+++ models.py
@@ -53,6 +53,6 @@ class Feedback(db.Model):
     __tablename__ = 'feedback'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullabe=False)
+    content = db.Column(db.Text, nullable=False)
     username = db.Column(db.String(20), db.ForeignKey(
         'users.username'), nullable=False)
